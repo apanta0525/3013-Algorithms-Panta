60449e10c072676873edae9ec7ca87594b8a9b4b
Aashish Panta <112501963+apanta0525@users.noreply.github.com>
2023-04-10 10:36:01 -0500

    Update main.cpp

--- Assignments/P01/main.cpp
+++ Assignments/P01/main.cpp
@@ -1,10 +1,10 @@
 /*****************************************************************
  *
- *  Author:           Aashish Panta
- *  Email:            liverpool.ashish.ap@gmail.com
+ *  Author:           Ashish Panta
+ *  Email:            apanta0525@gmail.com
  *  Label:            P01
  *  Title:            Database Indexes
- *  Course:           CMPS 3013
+ *  Course:           3013 Algorithm
  *  Semester:         Spring 2023
  *
  *  Description:
@@ -18,13 +18,12 @@
  *       N/A
  *
  *  Files:
- *       main.cpp                   : main driver program
- *       json.hpp                   : json parser library
- *       random_person_data.json    : json file with the data
+ *       main.cpp                   : driver program
+ *       json.hpp                   : json library
+ *       actual_person_data.json    : file with the 
+ * data
  *****************************************************************************/
-#include "PersonalDetails.cpp"
-#include "ListNode.cpp"
-#include "BST.cpp"
+
 #include "json.hpp"
 #include <algorithm>
 #include <bits/stdc++.h>
@@ -40,7 +39,253 @@ using namespace std;
 using json = nlohmann::json;
 
 /**
- * lower
+ * PersonalDetails
+ *
+ * Description:
+ *      This struct provides the structure for PersonalDetails data.
+ *
+ * Public Methods:
+ *                         N/A
+ *
+ * Private Methods:
+ *                          N/A
+ *
+ * Usage:
+ *      Provides a structure for PersonalDetails data.
+ *
+ */
+
+struct PersonalDetails {
+  int id;
+  string first_name;
+  string last_name;
+  string email;
+  string address;
+  string city;
+  string state;
+  double latitude;
+  double longitude;
+  string car;
+  string car_model;
+  string car_color;
+  string favorite_movie;
+  string pet;
+  string job_title;
+  string phone_number;
+  vector<string> stocks;
+};
+
+/**
+ * BST
+ *
+ * Description:
+ *      This struct provides the structure for Binary Search Tree Algo.
+ *
+ * Public Methods:
+ *                         N/A
+ *
+ * Private Methods:
+ *                          N/A
+ *
+ * Usage:
+ *      Provides a structure for binary search tree.
+ *
+ */
+
+struct BST {
+  string key;
+  PersonalDetails personalDetails;
+  BST *left;
+  BST *right;
+};
+
+/**
+ * insert
+ *
+ * Description:
+ *      The function adds new node in the binary search tree.
+ *
+ * Params:
+ *      BST                 :  root of the binary search tree.
+ *      PersonalDetails     :  data of the personalDetails to add in the node.
+ *      string              :  key to insert the data with
+ * Returns:
+ *      N/A
+ */
+
+void insert(BST *&root, PersonalDetails data, string key) {
+  if (root == NULL) {
+    root = new BST;
+    root->key = key;
+    root->personalDetails = data;
+    root->left = NULL;
+    root->right = NULL;
+  } else {
+    if (key < root->key) {
+      insert(root->left, data, key);
+    } else {
+      insert(root->right, data, key);
+    }
+  }
+}
+
+/**
+ * insert
+ *
+ * Description:
+ *      Overloaded insert. The function adds new node in the binary search tree
+ * with integer key.
+ *
+ * Params:
+ *      BST                  :  root of the binary search tree.
+ *      PersonalDetails      :  data of the personalDetails to add in the node.
+ *      int                  :  integer key to insert the data with
+ * Returns:
+ *      N/A
+ */
+
+void insert(BST *&root, PersonalDetails data, int key) {
+  if (root == NULL) {
+    root = new BST;
+    root->key = to_string(key);
+    root->personalDetails = data;
+    root->left = NULL;
+    root->right = NULL;
+  } else {
+    if (key < stoi(root->key)) {
+      insert(root->left, data, key);
+    } else {
+      insert(root->right, data, key);
+    }
+  }
+}
+
+/**
+ * insert
+ *
+ * Description:
+ *      Overloaded insert. The function adds new node in the binary search tree
+ * with the combination of longitude and latitude as the key.
+ *
+ * Params:
+ *      BST          :  root of the binary search tree.
+ *      PersonalDetails     :  data of the personalDetails to add in the node.
+ *      double       :  combination of longitude and latitude as a key to insert
+ * the data with
+ * Returns:
+ *   N/A
+ */
+
+void insert(BST *&root, PersonalDetails data, double key) {
+  if (root == NULL) {
+    root = new BST;
+    root->key = to_string(key);
+    root->personalDetails = data;
+    root->left = NULL;
+    root->right = NULL;
+  } else {
+    if (data.latitude + data.longitude < stod(root->key)) {
+      insert(root->left, data, key);
+    } else {
+      insert(root->right, data, key);
+    }
+  }
+}
+
+/**
+ * *search
+ *
+ * Description:
+ *       The function searches for a node in the binary
+ * search tree with integer key.
+ *
+ * Params:
+ *      BST          :  root of the BST
+ *      int          :  key to search the node with
+ * Returns:
+ *      BST          : node with the key
+ */
+
+BST *search(BST *root, int searchKey) {
+  static int count = 0;
+  count++;
+  if (!root) {
+    cout << endl;
+    cout << "Node count:" << count << endl;
+  } else if (root->personalDetails.id == searchKey) {
+    cout << endl;
+    cout << "Node count:" << count << endl;
+    return root;
+  } else if (searchKey < root->personalDetails.id) {
+    return search(root->left, searchKey);
+  } else {
+    return search(root->right, searchKey);
+  }
+}
+
+/**
+ * *search
+ *
+ * Description:
+ *       Overloaded *search function. The function searches for a node in the
+ * binary search tree with integer key.
+ *
+ * Params:
+ *      BST          :  root of the BST
+ *      string       :  string key to search the node with
+ * Returns:
+ *      BST          : node with the key
+ */
+
+BST *search(BST *root, string searchKey) {
+  static int count = 0;
+  count++;
+  if (root == NULL || root->key.find(searchKey) != string::npos) {
+    cout << endl;
+    cout << "Node count:" << count << endl;
+    return root;
+
+  } else if (searchKey < root->key) {
+    return search(root->left, searchKey);
+  } else {
+    return search(root->right, searchKey);
+  }
+}
+
+/**
+ * *search
+ *
+ * Description:
+ *       Overloaded *search function. The function searches for a node in the
+ * binary search tree with longitude and latitude as key.
+ *
+ * Params:
+ *      BST          :  root of the BST
+ *      double       :  latitude
+ *      double       :  longitude
+ * Returns:
+ *      BST          : node with the key combination of longitude and latitude
+ */
+
+BST *search(BST *root, double latitude, double longitude) {
+  static int count = 0;
+  count++;
+  if (root == NULL || root->personalDetails.latitude + root->personalDetails.longitude ==
+                          (latitude + longitude)) {
+    cout << endl;
+    cout << "Node count:" << count << endl;
+    return root;
+
+  } else if ((latitude + longitude) <
+             (root->personalDetails.latitude + root->personalDetails.longitude)) {
+    return search(root->left, latitude, longitude);
+  } else {
+    return search(root->right, latitude, longitude);
+  }
+}
+
+/**
+ * toLowerCase
  *
  * Description:
  *      The function converts the given string into lower case.
@@ -51,17 +296,17 @@ using json = nlohmann::json;
  * Returns:
  *      string     : string in lower case
  */
-string lower(string searchWord) {
+string toLowerCase(string searchWord) {
   transform(searchWord.begin(), searchWord.end(), searchWord.begin(),
             ::tolower);
   return searchWord;
 }
 
 /**
- * PersonalDetails
+ * ListNode
  *
  * Description:
- *      This struct provides the structure for personalDetails data.
+ *      This struct provides the structure for nodes in the linked list.
  *
  * Public Methods:
  *                         N/A
@@ -70,25 +315,157 @@ string lower(string searchWord) {
  *                          N/A
  *
  * Usage:
- *      Provides a structure for personalDetails data.
+ *      Provides a structure for linked list.
  *
  */
 
+struct ListNode {
+  PersonalDetails data;
+  struct ListNode *next;
+};
+
+/**
+ * insert_LinkedList
+ *
+ * Description:
+ *      The function adds new node in the linked list.
+ *
+ * Params:
+ *      ListNode     :
+ *      Employee     :  object of the struct Employee
+ * Returns:
+ *      N/A
+ */
+
+void insert_LinkedList(ListNode **root, PersonalDetails details) {
+  ListNode *new_node = new ListNode();
+  new_node->data = details;
+  new_node->next = NULL;
+  if (*root == NULL) {
+    *root = new_node;
+  } else {
+    ListNode *lastnode = *root;
+    while (lastnode->next != NULL) {
+      lastnode = lastnode->next;
+    }
+    lastnode->next = new_node;
+  }
+}
+
+/**
+ * searchList
+ *
+ * Description:
+ *      The function searches for a node in the linked list with string key.
+ *
+ * Params:
+ *      ListNode     :  node to search
+ *      string       :  key to search the node with
+ * Returns:
+ *      ListNode     : node being searched for
+ */
+
+ListNode *searchList(ListNode *node, string searchKey) {
+  int comp = 0;
+  if (node == NULL) {
+    return node;
+  }
+  while (node != NULL) {
+    comp++;
+     if(toLowerCase(node->data.first_name).find(searchKey) != string::npos ||
+        toLowerCase(node->data.last_name).find(searchKey) != string::npos ||
+        toLowerCase(node->data.email).find(searchKey) != string::npos ||
+        toLowerCase(node->data.address).find(searchKey) != string::npos ||
+        toLowerCase(node->data.city).find(searchKey) != string::npos ||
+        toLowerCase(node->data.state).find(searchKey) != string::npos ||
+        toLowerCase(node->data.car).find(searchKey) != string::npos ||
+        toLowerCase(node->data.phone_number).find(searchKey) != string::npos) {
+      cout << "Compared " << comp << " Nodes." << endl;
+      return node;
+    }
+    node = node->next;
+  }
+  cout << "Compared " << comp << " Nodes." << endl;
+  return node;
+}
+/**
+ * searchList
+ *
+ * Description:
+ *      Overloaded searchList. The function searches for a node in the linked
+ * list with integer key.
+ *
+ * Params:
+ *      ListNode     :  node to search
+ *      int          :  key to search the node with
+ * Returns:
+ *      ListNode     : node being searched for
+ */
+
+ListNode *searchList(ListNode *node, int search_key) {
+  int comp = 0;
+  if (node == NULL) {
+    return node;
+  }
+  while (node != NULL) {
+    comp++;
+    if (node->data.id == search_key) {
+      cout << "Compared " << comp << " Nodes." << endl;
+      return node;
+    }
+    node = node->next;
+  }
+  cout << "Compared " << comp << " Nodes." << endl;
+  return node;
+}
+
+/**
+ * searchList
+ *
+ * Description:
+ *      Overloaded searchList. The function searches for a node in the linked
+ * list with longitude and latitude.
+ * Params:
+ *      ListNode     :  node to search
+ *      int          :  latitude to search the node with
+ *      int          :  lngitude to search the node with
+ * Returns:
+ *      ListNode     : node being searched for
+ */
+
+ListNode *searchList(ListNode *node, double latitude, double longitude) {
+  int comp = 0;
+  if (node == NULL) {
+    return node;
+  }
+  while (node != NULL) {
+    comp++;
+    if (node->data.latitude == latitude && node->data.longitude == longitude) {
+      cout << "Compared " << comp << " Nodes." << endl;
+      return node;
+    }
+    node = node->next;
+  }
+  cout << "Compared " << comp << " Nodes." << endl;
+  return node;
+}
+
 int option;       // option choice for user to search data with
 string searchKey; // searchkey for the functions
+
 
 /**
  * choice
  *
  * Description:
  *      The function provides the user with all the fields that they can search
- * the personalDetails data with.
+ * the employee data with.
  * Params: N/A
  *
  * Returns:
  *      int     :  integer with the coice information
  */
-int choice() {
+int getChoice() {
   cout << "Select the field you want to search data on:\n"
        << "0.Id\n"
        << "1.First Name\n"
@@ -122,10 +499,10 @@ int main() {
   BST *bstGps = NULL;
   ListNode *linkedlist = NULL;
 
-  // fetch person data from the json file
+  // data from the json file
   ifstream file("random_person_data.json");
   string str;
-  vector<PersonalDetails> PersonalDetails; // vector of the PersonalDetails
+  vector<PersonalDetails> details; // vector of the PersonalDetails
   json j;
   file >> j;
 
@@ -136,7 +513,7 @@ int main() {
       stocks.push_back(elements.get<string>());
     }
 
-    PersonalDetails personalDetails = {j[i]["id"],
+    PersonalDetails detail = {j[i]["id"],
                          j[i]["first_name"],
                          j[i]["last_name"],
                          j[i]["email"],
@@ -153,31 +530,31 @@ int main() {
                          j[i]["job_title"],
                          j[i]["phone_number"],
                          stocks};
-    PersonalDetails.push_back(personalDetails);
+    details.push_back(detail);
   }
   // shuffling the data
-  shuffle(PersonalDetails.begin(), PersonalDetails.end(), random_device());
+  shuffle(details.begin(), details.end(), random_device());
 
   // inserting data into BST
-  for (auto personalDetails : PersonalDetails) {
-    insert(bstId, personalDetails, personalDetails.id);
-    insert(bstFname, personalDetails, lower(personalDetails.first_name));
-    insert(bstLname, personalDetails, lower(personalDetails.last_name));
-    insert(bstAddress, personalDetails, lower(personalDetails.address));
-    insert(bstEmail, personalDetails, lower(personalDetails.email));
-    insert(bstCity, personalDetails, lower(personalDetails.city));
-    insert(bstState, personalDetails, lower(personalDetails.state));
-    insert(bstCar, personalDetails, lower(personalDetails.car));
-    insert(bstPhone_number, personalDetails, lower(personalDetails.phone_number));
-    insert(bstGps, personalDetails, (personalDetails.latitude + personalDetails.longitude));
-    insert_LinkedList(&linkedlist, personalDetails);
+  for (auto data : details) {
+    insert(bstId, data, data.id);
+    insert(bstFname, data, toLowerCase(data.first_name));
+    insert(bstLname, data, toLowerCase(data.last_name));
+    insert(bstAddress, data, toLowerCase(data.address));
+    insert(bstEmail, data, toLowerCase(data.email));
+    insert(bstCity, data, toLowerCase(data.city));
+    insert(bstState, data, toLowerCase(data.state));
+    insert(bstCar, data, toLowerCase(data.car));
+    insert(bstPhone_number, data, toLowerCase(data.phone_number));
+    insert(bstGps, data, (data.latitude + data.longitude));
+    insert_LinkedList(&linkedlist, data);
   }
 
   int optionNum;
   int idSearchData;
-  optionNum = choice();
+  optionNum = getChoice();
 
-  // Timer Variables for displaying time spent
+  // timer variables
 
   auto t_start = std::chrono::high_resolution_clock::now();
   auto t_end = std::chrono::high_resolution_clock::now();
@@ -203,7 +580,7 @@ int main() {
     if (optionNum == 0) {
       idSearchData = stoi(searchKey);
     }
-    searchKey = lower(searchKey);
+    searchKey = (searchKey);
     t_start = std::chrono::high_resolution_clock::now();
     if (optionNum == 0) {
       e = search(bstId, idSearchData);
@@ -248,8 +625,7 @@ int main() {
       for (vector<string>::const_iterator i = e->personalDetails.stocks.begin();
            i != e->personalDetails.stocks.end(); ++i)
         stocks += *i + ", ";
-      
-      // Displaying the personal information of the user in
+      // Printing the information the user is looking for
       cout << "Details:" << endl;
       cout << "id : " << e->personalDetails.id
            << "\nfirst_name : " << e->personalDetails.first_name
@@ -271,7 +647,7 @@ int main() {
       cout << "Not Found" << endl; // if the information doesn't exist.
     }
     cout << endl;
-    optionNum = choice();
+    optionNum = getChoice();
   }
   return 0;
 }
